7dbb6b423d82aed84d10a278ea008ef8723106e3
mfseman <43916357+mfseman@users.noreply.github.com>
2018-11-25 19:01:57 -0500

    Fixing CharacterBox

--- src/gui/element/CharacterBox.java
+++ src/gui/element/CharacterBox.java
@@ -31,6 +31,7 @@ public class CharacterBox extends JTextField {
 	public CharacterBox() {
 		// textField used that sets the width of the textbox to three
 		setFont(new Font("Trebuchet MS", Font.PLAIN, 30));
+		setEditable(false);
 	
 		// Key stroke listener, when the user enters the enter key or up, down, left, right arrow keys
 		// the program will take the last character the user enters
@@ -43,12 +44,10 @@ public class CharacterBox extends JTextField {
 		    	}
 		    	else if (e == KeyEvent.VK_DELETE || e == KeyEvent.VK_CONTROL || 
 		    			e == KeyEvent.VK_ALT || e == KeyEvent.VK_WINDOWS ||
-		    			e == KeyEvent.VK_F || e == KeyEvent.VK_F2 || e == KeyEvent.VK_F3 ||
-		    			e == KeyEvent.VK_F5 || e == KeyEvent.VK_NUM_LOCK || e == KeyEvent.VK_CAPS_LOCK ||
-		    			e == KeyEvent.VK_ESCAPE || e == KeyEvent.VK_KP_DOWN || e == KeyEvent.VK_KP_UP ||
-		    			e == KeyEvent.VK_KP_LEFT || e == KeyEvent.VK_KP_RIGHT || e == KeyEvent.VK_ENTER ||
-		    			e == KeyEvent.VK_PAGE_DOWN || e == KeyEvent.VK_PAGE_UP || e == KeyEvent.VK_END ||
-		    			e == KeyEvent.VK_HOME || e == KeyEvent.VK_INSERT) {
+		    			e == KeyEvent.VK_NUM_LOCK || e == KeyEvent.VK_CAPS_LOCK ||
+		    			e == KeyEvent.VK_ESCAPE || e == KeyEvent.VK_ENTER ||
+		    			e == KeyEvent.VK_PAGE_DOWN || e == KeyEvent.VK_PAGE_UP || 
+		    			e == KeyEvent.VK_END || e == KeyEvent.VK_HOME || e == KeyEvent.VK_INSERT) {
 		    		setText("x");
 		    	}
 		    	else {
